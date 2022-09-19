5420e0478fcb74cbb546d0cc808d89e42c0a8a7e
Elisey <elisey.tagan@gmail.com>
2022-09-19 17:38:01 +0300

    Added glfw submodule

--- src/voxel_engine.cpp
+++ src/voxel_engine.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 
+#include <GLFW/glfw3.h>
+
+int WIDTH = 1280;
+int HEIGHT = 720;
+
 int main()
 {
-	std::cout << "Hello world!" << std::endl;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow* pWindow = glfwCreateWindow(WIDTH, HEIGHT, "Window", nullptr, nullptr);
+	if (pWindow == nullptr)
+	{
+		std::cerr << "Failed to create GLFW Window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(pWindow);
+
+	//glViewport(0, 0, WIDTH, HEIGHT);
+
+	while (!glfwWindowShouldClose(pWindow))
+	{
+		glfwPollEvents();
+		glfwSwapBuffers(pWindow);
+	}
+
+	glfwTerminate();
+
+	return 0;
 }
